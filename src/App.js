6f00672532a4022d327a41e96a6b46a0ee2336aa
mapcast <odri0220@naver.com>
2021-03-02 00:34:31 +0900

    ..

--- src/App.js
+++ src/App.js
@@ -8,16 +8,16 @@ function App() {
     <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route exact path ="/" component={MainPage}/>
-          <Route exact path ="/roadmap/1" component={Roadmap1}/>
-          <Route exact path ="/roadmap/2" component={Roadmap2}/>
-          <Route exact path ="/roadmap/3" component={Roadmap3}/>
-          <Route exact path ="/roadmap/4" component={Roadmap4}/>
-          <Route exact path ="/roadmap/5" component={Roadmap5}/>
-          <Route exact path ="/roadmap/6" component={Roadmap6}/>
-          <Route exact path ="/roadmap/7" component={Roadmap7}/>
-          <Route exact path ="/roadmap/8" component={Roadmap8}/>
-          <Route exact path ="/roadmap/9" component={Roadmap9}/>
-          <Route exact path ="/roadmap/10" component={Roadmap10}/>
+          <Route exact path ="/tmang-guide/roadmap/1" component={Roadmap1}/>
+          <Route exact path ="/tmang-guide/roadmap/2" component={Roadmap2}/>
+          <Route exact path ="/tmang-guide/roadmap/3" component={Roadmap3}/>
+          <Route exact path ="/tmang-guide/roadmap/4" component={Roadmap4}/>
+          <Route exact path ="/tmang-guide/roadmap/5" component={Roadmap5}/>
+          <Route exact path ="/tmang-guide/roadmap/6" component={Roadmap6}/>
+          <Route exact path ="/tmang-guide/roadmap/7" component={Roadmap7}/>
+          <Route exact path ="/tmang-guide/roadmap/8" component={Roadmap8}/>
+          <Route exact path ="/tmang-guide/roadmap/9" component={Roadmap9}/>
+          <Route exact path ="/tmang-guide/roadmap/10" component={Roadmap10}/>
         </Switch>
     </BrowserRouter>
   );
